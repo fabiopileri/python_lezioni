ba1de8c95cba287e679e8c51fba399e1813e416f
Fabio Pileri <pileri@olomedia.it>
2022-09-14 14:23:00 +0200

    Lezione 02

---
+++ Lezione 02/esercizi.py
@@ -0,0 +1,5 @@
+mia_eta = 43
+nome_utente = input('Come ti chiami? ')
+eta_utente = int(input('Quanti anni hai? '))
+somma_eta = mia_eta + eta_utente
+print('Ciao ' + nome_utente + ' benvenuto, sai che la somma della tua età e della mia età è ' + str(somma_eta))
